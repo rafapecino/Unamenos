67c486d816ffc404247fda4cc3f178f1f1d74076
rafapecino <rpecbon@myuax.com>
2023-03-26 20:18:01 +0200

    Ejercicio3

---
+++ src/main/java/ejercicio3/Main.java
@@ -0,0 +1,48 @@
+package ejercicio3;
+
+import java.util.Scanner;
+
+public class Main {
+    //Construya una función que convierta un número decimal en una cadena que represente el
+    //valor del número en hexadecimal (base 16) en java
+    public static String decimalToHexadecimal(int decimal){
+        String hexadecimal = "";
+        int resto;
+        while (decimal > 0){
+            resto = decimal % 16;
+            if (resto < 10){
+                hexadecimal = resto + hexadecimal;
+            }else{
+                switch (resto){
+                    case 10:
+                        hexadecimal = "A" + hexadecimal;
+                        break;
+                    case 11:
+                        hexadecimal = "B" + hexadecimal;
+                        break;
+                    case 12:
+                        hexadecimal = "C" + hexadecimal;
+                        break;
+                    case 13:
+                        hexadecimal = "D" + hexadecimal;
+                        break;
+                    case 14:
+                        hexadecimal = "E" + hexadecimal;
+                        break;
+                    case 15:
+                        hexadecimal = "F" + hexadecimal;
+                        break;
+                }
+            }
+            decimal = decimal / 16;
+        }
+        return hexadecimal;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca un numero decimal: ");
+        int decimal = sc.nextInt();
+        System.out.println(decimalToHexadecimal(decimal));
+    }
+}
