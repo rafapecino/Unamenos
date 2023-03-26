d7c70161d294ce0d2225be75413074a061b02b09
rafapecino <rpecbon@myuax.com>
2023-03-26 12:18:57 +0200

    Ejercicio1b)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,37 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double min = 4;
+        double max = 10;
+
+        double root = bisect(min, max);
+        System.out.println("La raíz es: " + root);
     }
+
+    public static final double N = 2;
+    public static final double PREC = 1e-6;
+
+    public static double f(double x) {
+        return x * x - N;
+    }
+
+    public static double bisect(double min, double max) {
+        double med = (min + max) / 2;
+        double diff = max - min;
+
+        while (diff >= PREC) {
+            if (f(min) * f(med) < 0) {
+                max = med;
+            } else {
+                min = med;
+            }
+            med = (min + max) / 2;
+            diff = max - min;
+        }
+
+        return med;
+    }
+    //En este ejemplo, el método bisect se utiliza para encontrar la raíz cuadrada del número N,
+    // que se define como una constante en la clase.
 }
